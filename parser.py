a5a2362c670080b517b3af644679288bc923999a
= <helenfoster2016@u.northwestern.edu>
2017-03-08 00:55:05 -0600

    WIP adds more work on gigaword parser

--- parser.py
+++ parser.py
@@ -1,51 +1,69 @@
 import xml.etree.ElementTree as ET
-import os.path.isdir as isdir
-import os.path.isfile as isfile
-import os.path.exists as exists
-import os.glob as glob
+import os
 import sys
+import gzip
 
 
+def build_dataset(input_dir, output_dir):
+    all_data_files = [os.path.join(input_dir,fn) for fn in next(os.walk(input_dir))[2]]
 
+    for i in range(0, len(all_data_files)):
+      parse_gigaword_file(all_data_files[i], i)
+    headlines = "headlines"
+    paragraphs = "paragraphs"
 
-def get_dir_files(path_to_dir):
-    pass
-
-
-def parse_gigaword_file(fname):
-    root = ET.parse(fname).getroot() 
+def parse_gigaword_file(fname, index):
+    print "parsing gigaword file number: " + str(index) 
+    print fname
+    f = gzip.open(fname, 'r')
+    root = ET.fromstring("<DATA>" + f.read().encode('utf-8') + "</DATA>")
+    headlines = ""
+    paragraphs = ""
 
     for doc in root:
-        headline, paragraph = parse_doc(doc)
+        content = parse_doc(doc)
+        if content:
+          headline, paragraph = content
+          headlines += headline.text + " .\n"
+          paragraphs += paragraph.text + " .\n"
+    
+    headlines_file = open("headlines-"+str(index), 'w')
+    headlines_file.write(headlines)
+    headlines_file.close()
 
+    paragraphs_file = open("paragraphs-"+str(index), 'w')
+    paragraphs_file.write(paragraphs)
+    paragraphs_file.close()
 
 def parse_doc(doc):
     headline   = doc.find("HEADLINE")
     text       = doc.find("TEXT")
-    paragraph  = text.findall("P")[0]
-
-    return (headline, paragraph)
-        
-    
-
-
-if __name__ == "__main__"
-    input_path = sys.argv[1] if len(sys.argv) >= 2 else "./input"
-    output_path = sys.argv[2] if len(sys.argv) >= 3 else "./output"
-    
-    if not exists(input_path):
-        sys.exit(0)
-
-    if not exists(output_path):
-        mkdir() # needs to be imported
-
-    if isdir(input_path):
-        files = get_dir_files(input_path)
-
-        for f in files:
-            output = parse_gigaword_file(f)
-            save_output(output, output_path)
+    paragraphs = text.findall("P")
+    if len(paragraphs) > 0:
+        paragraph  = text.findall("P")[0]
+        return (headline, paragraph)
     else:
+        return None
+
+if __name__ == "__main__":
+    build_dataset("input_data", "output_data")
+
+    #input_path = sys.argv[1] if len(sys.argv) >= 2 else "./input"
+    #output_path = sys.argv[2] if len(sys.argv) >= 3 else "./output"
+    
+    #if not exists(input_path):
+    #    sys.exit(0)
+
+    #if not exists(output_path):
+    #    mkdir() # needs to be imported
+
+    #if isdir(input_path):
+    #    files = get_dir_files(input_path)
+
+    #    for f in files:
+    #        output = parse_gigaword_file(f)
+    #        save_output(output, output_path)
+    #else:
         #isfile
-        output = parse_gigaword_file(input_path)
-        save_output(output, output_path)
+    #    output = parse_gigaword_file(input_path)
+    #    save_output(output, output_path)
