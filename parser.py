9e087b56e606fb4b47812fde576ea9c2e95d51f7
root <root@sliderz-2.c.valiant-cycle-850.internal>
2017-03-13 19:16:38 +0000

    Changed parser to get first sentence of first paragraph

--- parser.py
+++ parser.py
@@ -1,8 +1,12 @@
 import xml.etree.ElementTree as ET
+
+import re
 import os
 import sys
 import gzip
 
+# regex = r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s'
+regex = r"[\.?!]\s"
 
 def build_dataset(input_dir): 
     all_data_files = [os.path.join(input_dir,fn) for fn in next(os.walk(input_dir))[2]]
@@ -17,37 +21,50 @@ def parse_gigaword_file(fname, index):
       text = f.read()    
       root = ET.fromstring("<DATA>"+ text + "</DATA>")
     except:
+      print("Failed to open file")
       return
     headlines = ""
-    paragraphs = ""
+    sentences = ""
 
     for doc in root:
-        content = parse_doc(doc)
-        if content:
-          headline, paragraph = content
-          headlines += headline.text + " .\n"
-          paragraphs += paragraph.text + " .\n"
-    
-    headlines_file = open("training_data/headlines/headlines-"+str(index), 'w')
-    headlines_file.write(headlines)
+        try:
+	    content = parse_doc(doc)
+            if content is not None:
+                headline, sentence = content
+                headlines += headline.replace("\n", "") + "\n"
+                sentences += sentence.replace("\n", " ")+ " .\n"
+        except:
+            print("Parsed failed")
+
+    headlines_file = open("training_data/headlines/good-headlines-"+str(index), 'w')
+    headlines_file.write(headlines.encode("utf-8"))
     headlines_file.close()
 
-    paragraphs_file = open("training_data/paragraphs/paragraphs-"+str(index), 'w')
-    paragraphs_file.write(paragraphs)
-    paragraphs_file.close()
+    sentences_file = open("training_data/sentences/good-sentences-"+str(index), 'w')
+    sentences_file.write(sentences.encode("utf-8"))
+    sentences_file.close()
 
 def parse_doc(doc):
     headline   = doc.find("HEADLINE")
     text       = doc.find("TEXT")
     paragraphs = text.findall("P")
-    if (len(paragraphs) > 0) and (headline is not None):
-        paragraph  = text.findall("P")[0]
-        return (headline, paragraph)
-    else:
+    
+    if (len(paragraphs) > 0) and headline is not None:
+        paragraph      = text.findall("P")[0]
+	first_sentence = get_first_sentence(paragraph.text)
+        if first_sentence is not None:
+            return (headline.text, first_sentence)
+    return None
+
+
+def get_first_sentence(paragraph):
+    result = re.split(regex, paragraph, 1, re.MULTILINE)
+    if result is None:
         return None
+    return result[0]
 
 if __name__ == "__main__":
-    build_dataset("input_data")
+    build_dataset("better_sample")
 
     #input_path = sys.argv[1] if len(sys.argv) >= 2 else "./input"
     #output_path = sys.argv[2] if len(sys.argv) >= 3 else "./output"
